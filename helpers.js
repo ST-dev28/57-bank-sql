06101c189a1b932a923d011841dee6fe7ca665b0
SandraT <sndrbltr@gmail.com>
2021-09-25 09:40:32 +0000

    Helpers

--- helpers.js
+++ helpers.js
@@ -4,14 +4,21 @@ helpers.Capitalize = async (str) => {
     return str[0].toUpperCase() + str.slice(1);
 };
 
-helpers.formatDate = async (date) => {
+/*helpers.formatDate = async (date) => {
     const d = new Date(date);
     const dformat = [d.getFullYear(), d.getMonth() + 1, d.getDate()].join('-');
     return dformat;
 }
 helpers.formatTime = async (time) => {
-    const d = new Date(time);
-    const dformat = [d.getHours(), d.getMinutes() + 1, d.getSeconds()].join(':');
-    return dformat;
+    const t = new Date(time);
+    const tformat = [d.getHours(), d.getMinutes() + 1, d.getSeconds()].join(':');
+    return tformat;
+}*/
+
+helpers.fullDate = async (date) => {
+    const dt = new Date(date);
+    const fullDate = ([dt.getFullYear(), dt.getMonth() + 1, dt.getDate()].join('-')) + ' ' + ([dt.getHours(), dt.getMinutes() + 1, dt.getSeconds()].join(':'));
+    return fullDate;
 }
+
 module.exports = helpers;

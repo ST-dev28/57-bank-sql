60e846d5e543bdbb19356b354b23b5edc090e649
SandraT <sndrbltr@gmail.com>
2021-09-25 09:40:45 +0000

    History

--- History.js
+++ History.js
@@ -20,7 +20,32 @@ History.create = async (connection, transactionId, accountId, userId, amount) =>
         ) VALUES \
         (NULL, "'+ transactionId + '", "' + accountId + '", "' + userId + '", "' + amount + '", current_timestamp())';
     await connection.execute(sql);
-    return `Transactions has been created!`
+    return `Transaction history has been created!`
+}
+
+History.allTransactions = async (connection) => {
+    sql = 'SELECT `history`.`transactionId`,\
+            `history`.`accountId`,\
+            `transactions`.`transaction`,\
+            `users`.`firstname`,\
+            `users`.`lastname`,\
+            `date`\
+            FROM `users`\
+        LEFT JOIN `accounts`\
+            ON `users`.`id` = `accounts`.`userId`\
+        LEFT JOIN `history`\
+            ON `accounts`.`id` = `history`.`accountId`\
+        LEFT JOIN `transactions`\
+            ON `transactions`.`id` = `history`.`transactionId`';
+    const [list] = await connection.execute(sql);
+    //console.log(list);
+    let count = 0;
+    let transactionsList = [];
+    for (let { transaction, lastname, firstname, date, accountId } of list) {
+        transactionsList.push(`${++count}. Transaction - "${transaction}", user - ${firstname} ${lastname}, account number - ${accountId} \ntransaction date - ${await helpers.fullDate(date)}. Successful!`);
+    }
+    const resp = transactionsList.join('\n');
+    return await resp;
 }
 
 module.exports = History;
